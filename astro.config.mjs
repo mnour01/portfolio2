c4a016e4441b9e772a36c7942cd636e4096ebf49
Mehdi Nour <mehdi.nour@altergaz.local>
2025-05-09 12:22:12 +0200

    mise a jour du contenu 1/2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,17 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://renatokhael.com',
+  site: 'https://nourmehdidata.com',
   integrations: [tailwind(), react()],
 });
+
+
+
+// import { defineConfig } from "astro/config";
+// import gh from "@astrojs/github";
+
+// export default defineConfig({
+//   output: "static",
+//   base: "/kalifa-sankara/",       // Nom du repo GitHub
+//   integrations: [gh()],
+// });
